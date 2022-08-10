db2493c0235bb6079f617bf941c32305d7631959
Turkyeh yousef <91463018+Turkyeh@users.noreply.github.com>
2022-08-10 17:17:23 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+
+
+let person = prompt("Please enter your name", "Turkyeh");
+
+let Age = prompt("Please enter your age");
+
+ 
+if (Age< 0)
+{
+    Age = prompt("Please enter your age");
+}
+var gender1 =prompt("Please enter your gener  ");
+var male;
+var female;
+
+if (gender1 == male)
+{
+    alert("Welcome, MR. " + person + " \n im happy to visit my web page");
+}
+else if (gender1 == female)
+{
+    alert("Welcome, MS. " + person  + " \n im happy to visit my web page");
+}
+
+
+/*
+
+let gender = prompt("Please enter your gener \n fmail or mail ");
+let scop;
+ 
+if (gender ="fmail") {
+    scop = MS;
+
+}
+else if (gender == "Mail" || gender == "mail") {
+    scop = Mr;
+}
+else {
+    gender= prompt("Please enter your gener \n fmail or mail ");
+}
+
+
+if (scop=MS) {
+
+    alert("Hello " +MS  + person + " \n im happy to visit my web page");
+}
+else
+{    alert("Hello " +Mr + person + " \n im happy to visit my web page");
+
+    
+    }
+*/
+
+
+
+
